8c4497a7af4d2d06a0915f726b6465fb1caf451f
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-05-19 22:15:18 -0600

    Typed Letter in Alphabet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,34 @@
 /* Initializing game by creating empty fields */
 /* 3 will be changed to number randomizer based on word */
-let wordArray = []; 
+let wordArray = [];
 
-for (var i = 0; i < 3; i++){
-        wordArray[i] = "_";
-    }
+for (var i = 0; i < 3; i++) {
+    wordArray[i] = "_";
+}
 
 document.getElementById("emptyFields").innerHTML = wordArray.join(" "); /* Display blanks fields into page */
 
 
 
 /* Capturing User's Letters */
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let typedLetters = [];
 
-document.onkeyup = function(event){
-    typedLetters.push(event.key);/*  inputting each character typed into array */
-    console.log(typedLetters);
+document.onkeyup = function (event) {
+   
+    if (alphabet.includes(event.key)) {
+        typedLetters.push(event.key);/*  inputting each character typed into array */
+        console.log(typedLetters);
+    } 
+    else {
+        console.log("Not Valid Letter");
+    }
+ 
 }
 
- /*    for(var i = 0; i < 26; i++){
-            document.onkeyup = typedLetters.push;
-            console.log(typedLetters)
-        } */
-    
+/* document.onkeyup = function (event) {
+    event.key
+} */
+
+
+
