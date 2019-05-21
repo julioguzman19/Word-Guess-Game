f59a5e4656580d6757be256be23552989e5ef31c
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-05-20 21:56:39 -0600

    Confirming letter and if typed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,34 +5,34 @@
 
 /* Initializing game by creating empty fields */
 /* 3 will be changed to number randomizer based on word */
-let wordArray = [];
+let guessingWord= [];
 let words = ["goku","frieza"];
 let word = words[Math.floor(Math.random() * words.length)];
-
-for (var i = 0; i < 4; i++) {
-    wordArray[i] = "_";
-}
-
-document.getElementById("emptyFields").innerHTML = wordArray.join(" "); /* Display blanks fields into page */
-
-
-
-/* Capturing User's Letters */
+let wordChars = word.length;
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let typedLetters = [];
 
+for (var i = 0; i < wordChars; i++) {
+    guessingWord[i] = "_";
+}
+
+document.getElementById("emptyFields").innerHTML = guessingWord.join(" "); /* Display blanks fields into page */
+
+/* Capturing User's Letters */
 document.onkeyup = function (event) {
    
     if (alphabet.includes(event.key) && !typedLetters.includes(event.key) && word.includes(event.key)) { /* checking valid letter and not already typed and if correct */
+
+   
         
-        
-        
-        typedLetters.push(event.key);/*  inputting each character typed into array */
-        console.log(typedLetters);
-        document.getElementById("guessedLetters").innerHTML = typedLetters.join(" ");
     } 
-    else {
-        alert("Not Valid Letter or already guessed");
+    else if (alphabet.includes(event.key) && !typedLetters.includes(event.key)) { /* checking valid letter and not already typed */
+        alert("Guessed letter");
+        typedLetters.push(event.key);
+        document.getElementById("guessedLetters").innerHTML = typedLetters.join(" ");  /*  inputting each character typed into array */
+    }
+    else{
+        alert("Not a valid character or already guessed");
     }
  
 }
